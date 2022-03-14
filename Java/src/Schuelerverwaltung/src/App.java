267a62f6960e22f0dc9369df9c55174bc5e63122
Mightiqaler <niklas.schoepper@hebk.de>
2022-03-14 10:56:43 +0100

    s

--- Java/src/Schuelerverwaltung/src/App.java
+++ Java/src/Schuelerverwaltung/src/App.java
@@ -1,12 +1,12 @@
 public class App {
     public static void main(String[] args) throws Exception {
-      //  Schuelerverwaltung app1 = new Schuelerverwaltung();
+        //  Schuelerverwaltung app1 = new Schuelerverwaltung();
 
-//GUI2 gui = new GUI2();
-SystemController s = new SystemController();
-Input input = new Input();
+        //GUI2 gui = new GUI2();
+        SystemController s = new SystemController();
+        Input input = new Input();
 
-ArraysList a = new ArraysList();
+        ArraysList a = new ArraysList();
         
     }
 }
